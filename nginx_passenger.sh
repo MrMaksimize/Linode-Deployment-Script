e4c7be78a82f7797488672d6a9ba082cb91d52fd
Maksim Pecherskiy <max@maksimize.com>
2011-12-26 17:15:18 -0600

    trying stuff

--- nginx_passenger.sh
+++ nginx_passenger.sh
@@ -1,16 +1,18 @@
 echo "nginx compile and install passenger"
 # Nginx to serve webs
 #aptitude -y install libpcre3 libpcre3-dev libpcrecpp0 libssl-dev zlib1g-dev
-gem install passenger
-passenger-install-nginx-module
-cat /home/$USER/deployment/config_files/nginx_passenger_init_d.txt > /etc/init.d/nginx
+#gem install passenger
+#passenger-install-nginx-module
+echo $DEPLOYMENT_PATH
+cat $DEPLOYMENT_PATH/config_files/nginx_passenger_init_d.txt > /etc/init.d/nginx
 chmod +x /etc/init.d/nginx && /usr/sbin/update-rc.d -f nginx defaults
 mv /opt/nginx/conf/nginx.conf /opt/nginx/conf/nginx.conf.bak
-cat /home/$USER/deployment/config_files/nginx_conf_passenger.txt > /opt/nginx/conf/nginx.conf
+cat $DEPLOYMENT_PATH/config_files/nginx_conf_passenger.txt > /opt/nginx/conf/nginx.conf
 mkdir /opt/nginx/sites-available /opt/nginx/sites-enabled
-cat /home/$USER/deployment/config_files/default_vhost_passenger.txt > /opt/nginx/sites-available/default
+cat $DEPLOYMENT_PATH/config_files/default_vhost_passenger.txt > /opt/nginx/sites-available/default
 ln -s /opt/nginx/sites-available/default /opt/nginx/sites-enabled/default
 #webdir structure
+echo $USER
 mkdir /home/$USER/public_html
 addgroup webmasters 
 usermod -G webmasters www-data 
