6359d1b099e8fe456d9b79c56dee5550187e8940
Maksim Pecherskiy <max@maksimize.com>
2011-12-27 17:29:05 -0600

    cleaned up old stuff

--- deprecated/authkeys5.sh
+++
@@ -1,7 +0,0 @@
-echo "authkeys"
-# Setup authentication keys for easy secure login
-mkdir /home/$USER/.ssh
-echo $PUBLIC_KEY >> /home/$USER/.ssh/authorized_keys
-chown -R $USER:$USER /home/$USER/.ssh
-chmod 700 /home/$USER/.ssh
-chmod 600 /home/$USER/.ssh/authorized_keys
